057c72c1db8b0c1ad436ea704648aba1d04014a6
hehaowen00 <edmundhe01@gmail.com>
2025-05-22 08:39:18 +1000

    [message-queue] wip

--- message-queue/producer.go
+++ message-queue/producer.go
@@ -1,10 +1,17 @@
 package messagequeue
 
+import (
+	"errors"
+	"fmt"
+)
+
+var ErrTopicNotFound = errors.New("topic not found")
+
 type Publisher struct {
 	ex *Exchange
 }
 
-func (p *Publisher) Publish(topic string, msg []byte) error {
+func (p *Publisher) Publish(topic string, msg []byte) (string, error) {
 	p.ex.rw.RLock()
 	t, ok := p.ex.topics[topic]
 	if !ok {
@@ -14,12 +21,16 @@ func (p *Publisher) Publish(topic string, msg []byte) error {
 		err := p.ex.CreateTopic(topic)
 		if err != nil {
 			p.ex.rw.Unlock()
-			return err
+			return "", fmt.Errorf("error creating topic - %w", err)
 		}
 
 		t, ok = p.ex.topics[topic]
 	} else {
 		p.ex.rw.RUnlock()
+	}
+
+	if !ok {
+		return "", ErrTopicNotFound
 	}
 
 	return t.Send(msg)
