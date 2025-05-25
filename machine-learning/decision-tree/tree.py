7e65118db4155152d37670af7bbb4136b4440c06
hehaowen00 <edmundhe01@gmail.com>
2025-05-25 17:45:50 +1000

    add decision tree implementation

---
+++ machine-learning/decision-tree/tree.py
@@ -0,0 +1,222 @@
+import pandas as pd
+import numpy as np
+import math
+from collections import Counter
+from sklearn.model_selection import train_test_split
+
+def entropy(y):
+    total = len(y)
+    counts = Counter(y)
+    ent = 0.0
+
+    for count in counts.values():
+        p = count / total
+        ent -= p * math.log2(p)
+
+    return ent
+
+def tokenize(arr):
+    lut = {}
+    res = []
+
+    for v in arr:
+        if v not in lut:
+            lut[v] = len(lut) + 1
+        res.append(lut[v])
+
+    return lut, res
+
+def info_gain(y_parent, y_left, y_right):
+    parent = entropy(y_parent)
+    left = entropy(y_left)
+    right = entropy(y_right)
+
+    n = len(y_parent)
+    n_left = len(y_left)
+    n_right = len(y_right)
+    weighted = (n_left/n) * left + (n_right/n) * right
+
+    return parent - weighted
+
+def gain_ratio(X, a):
+    pass
+
+def gini(y):
+    counts = y.value_counts()
+    probs = counts / len(y)
+    return 1 - sum(p ** 2 for p in probs)
+
+def gini_index(y_parent, y_left, y_right):
+    parent = gini(y_parent)
+    left = gini(y_left)
+    right = gini(y_right)
+
+    n = len(y_parent)
+    n_left = len(y_left)
+    n_right = len(y_right)
+
+    if n_left == 0 or n_right == 0:
+        return 0
+
+    weighted = (n_left/n) * left + (n_right/n) * right
+    return parent - weighted
+
+class Node:
+    def __init__(self, criterion) -> None:
+        self.left = None
+        self.right = None
+        self.feature = None
+        self.value = None
+        self.is_leaf = False
+        self.prediction = None
+        self.gain = None
+        self.criterion = criterion
+
+    def fit(self, X, y):
+        if len(set(y)) == 1:
+            self.is_leaf = True
+            self.prediction = y.iloc[0]
+            return
+
+        if len(X.columns) == 0:
+            self.is_leaf = True
+            self.prediction = y.mode()[0]
+            return
+        
+        best_gain = -math.inf
+        best_feature = None
+        best_threshold = None
+
+        for feature in X.columns:
+            # print(feature)
+            values = sorted(X[feature].unique())
+            possible_thresholds = [(v1+v2)/2 for v1, v2 in zip(values[:-1], values[1:])]
+            # print('thesholds\n', feature, possible_thresholds)
+
+            for threshold in possible_thresholds:
+                left_mask = X[feature] <= threshold
+                right_mask = X[feature] > threshold
+
+                y_left = y[left_mask]
+                y_right = y[right_mask]
+
+                if len(y_left) == 0 or len(y_right) == 0:
+                    continue
+                
+                gain = self.criterion(y, y_left, y_right)
+                # print(feature, gain, best_gain)
+                if gain > best_gain:
+                    best_gain = gain
+                    best_feature = feature
+                    best_threshold = threshold
+
+        if best_gain == 0 or best_feature is None:
+            self.is_leaf = True
+            self.prediction = y.mode()[0]
+            return
+        
+        self.feature = best_feature
+        self.value = best_threshold
+        self.gain = best_gain
+
+        left_mask = X[best_feature] <= best_threshold
+        right_mask = X[best_feature] > best_threshold
+
+        X_left, y_left = X[left_mask], y[left_mask]
+        X_right, y_right = X[right_mask], y[right_mask]
+
+        self.left = Node(self.criterion)
+        self.left.fit(X_left, y_left)
+
+        self.right = Node(self.criterion)
+        self.right.fit(X_right, y_right)
+
+    def predict_row(self, row):
+        if self.is_leaf:
+            return self.prediction
+
+        if row[self.feature] <= self.value:
+            return self.left.predict_row(row)
+        else:
+            return self.right.predict_row(row)
+    
+class DecisionTreeClassifier:
+    def __init__(self, criterion: str = 'information') -> None:
+        self.root = None
+        lut = {
+            'information': info_gain,
+            'gini': gini_index,
+        }
+        self.criterion = lut[criterion]
+        if self.criterion is None:
+            raise Exception('criterion not found: ' + criterion)
+
+    def fit(self, X, y):
+        self.root = Node(self.criterion)
+        self.root.fit(X, y)
+
+    def predict(self, X):
+        return X.apply(lambda row: self.root.predict_row(row), axis=1)
+
+    def print(self, tree=None, indent=' '):
+        if not tree:
+            tree = self.root
+
+        # if len(indent) > 20:
+        #     raise Exception('wth')
+
+        if tree.is_leaf:
+            print(tree.prediction)
+        else:
+            print('X_' + str(tree.feature), '<=', tree.value, '?', tree.gain)
+            print('%sleft:' % (indent), end='')
+            self.print(tree.left, indent + ' ')
+            print('%sright:' % (indent), end='')
+            self.print(tree.right, indent + ' ')
+
+if __name__ == '__main__':
+    samples = ['c1', 'c2', 'c3', 'c4', 'nc1', 'nc2', 'nc3']
+    classification = ['c', 'c', 'c', 'c', 'nc', 'nc', 'nc']
+    m1 = [1,1,1,0,0,0,1]
+    m2 = [1,1,0,1,0,1,1]
+    m2 = [1,0,1,1,0,0,0]
+    m3 = [0,1,1,0,0,0,0]
+
+    df = pd.DataFrame({
+        'id': samples,
+        'm1': m1,
+        'm2': m2,
+        'm3': m3,
+        'classification': classification,
+    })
+    print(df)
+
+    X = df[['m1', 'm2', 'm3']]
+    y = df['classification']
+
+    # lut, y0 = tokenize(df['classification'])
+    # print(X)
+    # print(list(y), lut, y0)
+
+    # x_train, x_test, y_train, y_test = train_test_split(X, y, random_state=42)
+    x_train = X
+    y_train = y
+    
+    # print(len(x_train),  len(y_train))
+    # print(x_test, y_test)
+
+    # ent = info_gain(df[['m1', 'm2', 'm3']], df['classification'], 'm1')
+    # print(ent)
+
+    # ent = info_gain(df[['m1', 'm2', 'm3']], df['classification'], 'm2')
+    # print(ent)
+
+    # ent = info_gain(df[['m1', 'm2', 'm3']], df['classification'], 'm3')
+    # print(ent)
+
+    tree = DecisionTreeClassifier('gini')
+    tree.fit(x_train, y_train)
+    res = tree.predict(X)
+
+    print('x_test\n', res)
+    print('y_test\n', y)
