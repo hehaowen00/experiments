beca3f16113cb4d1bc3eeb13091f90df75f79e3b
hehaowen00 <edmundhe01@gmail.com>
2025-05-25 21:21:06 +1000

    titanic - parse ticket column

--- machine-learning/decision-tree/titanic.py
+++ machine-learning/decision-tree/titanic.py
@@ -24,7 +24,7 @@ def encode_data(data):
     enc = LabelEncoder()
     data['Sex'] = enc.fit_transform(data['Sex'])
     data['Embarked'] = enc.fit_transform(data['Embarked'])
-    data['Ticket'] = enc.fit_transform(data['Ticket'])
+    # data['Ticket'] = enc.fit_transform(data['Ticket'])
     # data['Cabin'] = enc.fit_transform(data['Cabin'])
     data['Age'] = data['Age'].dropna()
     data['Age'] = data['Age'].fillna(data['Age'].median())
@@ -33,6 +33,9 @@ def encode_data(data):
     data["Deck"] = data["Cabin"].str.slice(0,1)
     data["Room"] = data["Cabin"].str.slice(1,5).str.extract("([0-9]+)", expand=False).astype("float")
     data["Room"] = data["Room"].fillna(data["Room"].mean())
+    data[['TicketCategory', 'TicketNumber']] = data['Ticket'].str.extract(r'([A-Za-z./\d]+)?\s*(\d+)$')
+    data['TicketCategory'] = enc.fit_transform(data['TicketCategory'])
+    data['TicketNumber'] = data['TicketNumber'].astype(float)
     data = one_hot(data, ['Deck'], drop_col=True)
     return data
 
@@ -42,11 +45,13 @@ if __name__ == '__main__':
     train_data = encode_data(train_data)
     test_data  = encode_data(test_data)
     print(train_data.head())
+    print(train_data.info())
+    print(train_data['TicketNumber'].head())
 
     # train_data['Deck'] = enc.fit_transform(train_data['Deck'])
 
     decks = [col for col in test_data.columns if col.startswith('Deck_')]
-    train_cols = ['SibSp', 'Sex', 'Parch', 'Fare', 'Pclass', 'Embarked', 'Ticket', 'Cabin_Cat', 'Room']
+    train_cols = ['SibSp', 'Sex', 'Parch', 'Fare', 'Pclass', 'Embarked', 'TicketCategory', 'TicketNumber', 'Cabin_Cat', 'Room']
     train_cols.extend(decks)
     X = train_data[train_cols]
     y = train_data['Survived']
